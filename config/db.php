6bb3825ea7d40d8ed6dc7dd319746206a939acab
RedHood <red@hood.su>
2020-08-03 21:29:53 +1000

    Установка шаблона Basic, установка phpdotenv

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+use \Dotenv\Dotenv;
+
+Dotenv::createUnsafeImmutable(__DIR__ . '/../')->load();
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn'       => getenv('DB_TYPE').':host='.getenv('DB_HOST').';dbname='.getenv('DB_BASE').';port='.getenv('DB_PORT'),
+    'username'  => getenv('DB_NAME'),
+    'password'  => getenv('DB_PSWD'),
+    'charset'   => getenv('DB_CHAR'),
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
